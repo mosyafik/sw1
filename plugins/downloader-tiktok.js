8c47ea1f85af2e0426b672babce9577b2461c6a0
Mosyafik Jr <63044745+mosyafik@users.noreply.github.com>
2022-10-10 13:42:43 +0700

    Update downloader-tiktok.js
    import axios from 'axios'
    let handler = async (m, { conn, args, usedPrefix, command }) => {
        if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
        let res = (await axios.get(API('males', '/tiktok', { url: args[0] } ))).data;
        if (res.status != 200) throw res.message;
        if (!res) throw res.message;
        conn.sendButtonVid(m.chat, res.video, `
    *「 🇹 ᴛ ɪ ᴋ ᴛ ᴏ ᴋ 」*
                     ████████▀▀▀████
                     ████████────▀██
                     ████████──█▄──█
                     ███▀▀▀██──█████
                     █▀──▄▄██──█████
                     █──█████──█████
                     █▄──▀▀▀──▄█████
                     ███▄▄▄▄▄███████
    ────────── ⇆ㅤ◁ㅤ ❚❚ㅤ ▷ㅤ↻ ──────────/n
    *Tittle:* ${res.title}\n${res.author ? `*Video By:* ${res.author}` : '\n' }`.trim(), 'Cara Simpan di galeri :\n1. Download dulu videonya\n2. Buka dan klik titik 3 pojok kanan atas\n3. lalu klik simpan!\n\n*BotShin v2*', 'menu', usedPrefix + 'menu', m)
    }
    handler.help = ['tiktok'].map(v => v + ' <url>')
    handler.tags = ['downloader']
    handler.command = /^(tiktok?)$/i
    
    export default handler

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,1 +1,25 @@
+import axios from 'axios'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+    let res = (await axios.get(API('males', '/tiktok', { url: args[0] } ))).data;
+    if (res.status != 200) throw res.message;
+    if (!res) throw res.message;
+    conn.sendButtonVid(m.chat, res.video, `
+*「 🇹 ᴛ ɪ ᴋ ᴛ ᴏ ᴋ 」*
+                 ████████▀▀▀████
+                 ████████────▀██
+                 ████████──█▄──█
+                 ███▀▀▀██──█████
+                 █▀──▄▄██──█████
+                 █──█████──█████
+                 █▄──▀▀▀──▄█████
+                 ███▄▄▄▄▄███████
+────────── ⇆ㅤ◁ㅤ ❚❚ㅤ ▷ㅤ↻ ────────── \n
+*Judul:* ${res.title}\n
+${res.author ? `*Pembuat Video:* ${res.author}` : '\n' }`.trim(), 'Cara Save di galery:\n1. Download dulu videonya\n2. Buka terus klik titik 3 pojok kanan atas\n3. lalu klik simpan!\n\n *BotShin v2*', 'menu', usedPrefix + 'menu', m)
+}
+handler.help = ['tiktok'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^(tiktok?)$/i
 
+export default handler
